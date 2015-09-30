d5af7e5659d3a73b6f4062e58f81e3718ff181fe
ttasterisco <ttasterisco@gmail.com>
2015-09-30 19:11:27 +0800

    cleaning up jwt

--- src/mx/bodyguard/jwt.clj
+++ src/mx/bodyguard/jwt.clj
@@ -11,7 +11,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ; given a claim and a secret, builds the jwtoken
-(defn create-jwt [claim secret]
+(defn create-jwt [secret claim]
   (->
     claim
     (jwt/jwt)
@@ -48,7 +48,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ; extracts and decodes the token from the auth header and stores it in the request map
-(defn wrap-token [handler error-handler]
+(defn wrap-token-to-params [handler error-handler]
   (fn [request]
     (try+
       (->>
@@ -59,18 +59,11 @@
     (catch Object e
       (error-handler handler request e)))))
 
-; this middleware expects wrap-token to have ran before (it adds the :jwt to the request map)
-(defn wrap-verify [handler secret on-error]
-  (fn [request]
-    (if (valid-jwt? (:jwt request) secret)
-      (handler request)
-      (on-error handler secret request))))
-
 ; this middleware extracts data from the jwt-oken's claims and adds it into the request's :params
 ; useful for acessing the params without having to dig into the jwt
 ; ATTN: fails silently; assumes token is valid and all ks are present (will select available ks)
 ; will continue processing if token is not present
-(defn wrap-claims-to-params [handler ks on-error]
+(defn wrap-claims-to-params [handler ks]
   (fn [request]
     (if-let [token (:jwt request)]
       ; token is present, extract claims, the fields we want, add them to the request and continue
